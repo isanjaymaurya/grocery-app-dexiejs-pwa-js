84fb552f2d7c3d151a91df44cb4c6e00b1956b33
isanjaymaurya <its.sanjaymaurya@gmail.com>
2022-09-10 14:31:57 +0530

    added new icons for manifist file

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@
 const db = new Dexie('GroceryApp')
 
 db.version(1).stores({
-    items: '++id,name,price,isPurchased'
+  items: '++id,name,price,isPurchased'
 })
 
 const itemInput = document.getElementById('itemInput')
@@ -12,10 +12,10 @@ const totalPriceDiv = document.getElementById('totalPriceDiv')
 const createNewListDiv = document.getElementById('createNewListDiv')
 
 const poplateNewItemsDiv = async () => {
-    const allItems = await db.items.reverse().toArray()
-
-    newItemsDiv.innerHTML = allItems.map(item => `
-    <div class="item ${item.isPurchased && 'purchased'}">
+  const allItems = await db.items.reverse().toArray()
+  console.table(allItems)
+  newItemsDiv.innerHTML = allItems.map(item => `
+    <div class="item ${item.isPurchased ? 'purchased' : ''}">
       <input
         type="checkbox"
         class="checkbox"
@@ -25,26 +25,30 @@ const poplateNewItemsDiv = async () => {
       <div class="itemInfo">
         <p>${item.name}</p>
         <p>₹${item.price} x ${item.qty}</p>
+        <p>₹${item.price * item.qty}</p>
       </div>
 
-      ${!item.isPurchased ? `<div class="itemChange">
+    <div class="itemChange">
         <button onclick="deleteItem(${item.id})" class="deleteButton">
         <span class="deleteSign">&#9747</span>
         </button>
-      </div>`: ``}
+      </div>
     </div>
   `).join('')
 
-  const arrayOfPrices = allItems.map(i => i.price * item.qty)
+  const arrayOfPrices = allItems.map(i => i.price * i.qty)
+  console.log("arrayOfPrices>>>", arrayOfPrices)
   const totalPrice = arrayOfPrices.reduce((a, b) => a + b, 0)
   totalPriceDiv.innerHTML = `Total Price: ₹ ${totalPrice}`
 }
 
 // add toggle item status
 const toggleItemStatus = async (e, id) => {
-  await db.items.update(id, {isPurchased: !!e.target.checked})
+  await db.items.update(id, {
+    isPurchased: !!e.target.checked
+  })
   await poplateNewItemsDiv()
-} 
+}
 
 // delete items from db
 const deleteItem = async id => {
@@ -61,33 +65,33 @@ const deleteAllItems = () => {
 }
 
 itemInput.onsubmit = async (e) => {
-    e.preventDefault()
-    const name = document.getElementById('nameInput').value
-    const qty = document.getElementById('qtyInput').value
-    const price = document.getElementById('priceInput').value
+  e.preventDefault()
+  const name = document.getElementById('nameInput').value
+  const qty = document.getElementById('qtyInput').value
+  const price = document.getElementById('priceInput').value
 
-    await db.items.add({
-        name,
-        qty,
-        price
-    })
+  await db.items.add({
+    name,
+    qty,
+    price
+  })
 
-    await poplateNewItemsDiv()
-    itemInput.reset()
+  await poplateNewItemsDiv()
+  itemInput.reset()
 
-    Toastify({
-        text: `${name} Added`,
-        duration: 3000,
-        newWindow: true,
-        close: true,
-        gravity: "bottom", // `top` or `bottom`
-        position: "center", // `left`, `center` or `right`
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        style: {
-            background: "linear-gradient(to right, #00b09b, #96c93d)",
-        },
-        onClick: function () {} // Callback after click
-    }).showToast();
+  Toastify({
+    text: `${name} Added`,
+    duration: 3000,
+    newWindow: true,
+    close: true,
+    gravity: "bottom", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function () {} // Callback after click
+  }).showToast();
 }
 
 window.onload = poplateNewItemsDiv
